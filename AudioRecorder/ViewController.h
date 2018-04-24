1938955da3fc84f28ea21eadc20a33755a4ae318
builder <builder@builderdemacbook-pro.local>
2018-04-24 10:44:02 +0800

    Initial Commit

---
+++ AudioRecorder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AudioRecorder
+//
+//  Created by builder on 2018/4/24.
+//  Copyright © 2018年 builder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AudioRecorder/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AudioRecorder
+//
+//  Created by builder on 2018/4/24.
+//  Copyright © 2018年 builder. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
